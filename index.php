19b5039b2e9a6b167159be35fcc228e51cd6c93b
ramospresley <ramospresley@gmail.com>
2026-03-14 22:25:04 -0300

    alteração de titulo da página

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $servicos = $stmt->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Barbershop Premium - Agendamento Online</title>
+    <title>Barbearia Premium - Agendamento Online</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700;900&family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
 </head>
